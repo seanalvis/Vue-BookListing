2819469c442cba25f840a5e216922d2cc1db4136
Sean Alvis <me@seanalvis.com>
2019-06-23 12:22:00 -0400

    Completed the module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -3,7 +3,7 @@
     <div>
         <h1>{{ title }}</h1>
         <ul>
-            <li v-for="book in books">{{book.title}} : {{book.author}}</li>
+            <li v-for="book in books"></li>
         </ul>
     </div>
 </template>
@@ -34,11 +34,6 @@ h1, h2 {
 ul {
     list-style-type: none;
     padding: 0;
-}
-
-li {
-    display: block;
-    margin: 0 10px;
 }
 
 </style>
